51d2b3a76c4403496191f2f0d74fdf2292d42e6b
chrisgalo09@hotmail.com <Chrisgalo>
2018-03-13 00:19:25 -0600

    Commit Inicial

---
+++ app/src/main/java/com/example/christiangarcia/pruebabd/AdminSQLiteOpenHelper.java
@@ -0,0 +1,38 @@
+package com.example.christiangarcia.pruebabd;
+
+/**
+ * Created by Christian Garcia on 18/02/2018.
+ */
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class AdminSQLiteOpenHelper  extends SQLiteOpenHelper{
+
+    public AdminSQLiteOpenHelper(Context context, String nombre, SQLiteDatabase.CursorFactory factory, int version) {
+
+        super(context, nombre, factory, version);
+
+    }
+
+    @Override
+
+    public void onCreate(SQLiteDatabase db) {
+
+        //aquí creamos la tabla de usuario (dni, nombre, ciudad, numero)
+        db.execSQL("create table usuario(dni integer primary key, nombre text, ciudad text, numero integer)");
+
+    }
+
+    @Override
+
+    public void onUpgrade(SQLiteDatabase db, int version1, int version2) {
+
+        db.execSQL("drop table if exists usuario");
+
+        db.execSQL("create table usuario(dni integer primary key, nombre text, ciudad text, numero integer)");
+
+    }
+
+}
